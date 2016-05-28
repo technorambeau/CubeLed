bf623c3538a1123e1b0cbc3f986fab082d354337
technorambeau <s.rambeau@outlook.com>
2016-05-28 02:48:29 +0200

    V 0.001

--- ProgrammeArduino/LedCubeLouise/LedCubeLouise.ino
+++ ProgrammeArduino/LedCubeLouise/LedCubeLouise.ino
@@ -19,41 +19,42 @@ int etage3 = 12;
  * 
  * 
  * 
- *                   |25|___________________|26|___________________|27|
- *                   |  /                   |  /                   |  /
- *                   |/ |                   |/ |                   |/ |
- *                  /|  |                  /|  |                  /|  |
- *            |22|/__|__|____________|23|/__|__|____________|24|/  |  |
- *            | /|   |  |            | /|   |  |            | /|   |  |
- *            /  |   |  |            /  |   |  |            /  |   |  |
- *          / |  |   |  |          / |  |   |  |          / |  |   |  |
- *     |19/___|__|___|__|_____|20/___|__|___|__|_____|21/   |  |   |  |
- *     |  |   |  |   |  |     |  |   |  |   |  |     |  |   |  |   |  |
- *     |  |   |  |   |16|_____|__|___|__|___|17|_____|__|___|__|___|18|
- *     |  |   |  |   |  /     |  |   |  |   |  /     |  |   |  |   |  /
- *     |  |   |  |   |/ |     |  |   |  |   |/ |     |  |   |  |   |/ |
- *     |  |   |  |  /|  |     |  |   |  |  /|  |     |  |   |  |  /|  |
- *     |  |   |13|/__|__|_____|__|___|14|/__|__|_____|__|___|15|/  |  |
- *     |  |   | /|   |  |     |  |   | /|   |  |     |  |   | /|   |  |
- *     |  |   /  |   |  |     |  |   /  |   |  |     |  |   /  |   |  |
- *     |  | / |  |   |  |     |  | / |  |   |  |     |  | / |  |   |  |
- *     |10/___|__|___|__|_____|11/___|__|___|__|_____|12/   |  |   |  |
- *     |  |   |  |   |  |     |  |   |  |   |  |     |  |   |  |   |  |
- *     |  |   |  |   |7 |_____|__|___|__|___|8 |_____|__|___|__|___|9 |
- *     |  |   |  |      /     |  |   |  |      /     |  |   |  |      /
- *     |  |   |  |    /       |  |   |  |    /       |  |   |  |    /
- *     |  |   |  |  /         |  |   |  |  /         |  |   |  |  /
- *     |  |   |4 |/___________|__|___|5 |/___________|__|___|6 |/
- *     |  |     /             |  |     /             |  |     /
- *     |  |   /               |  |   /               |  |   /
- *     |  | /                 |  | /                 |  | /
- *     |1 /___________________|2 /___________________|3 /
+ *                     25_____________________26_____________________27
+ *                      /                      /                      /
+ *                    / |                    / |                    / |
+ *                  /   |                  /   |                  /   |
+ *              22/_____|_____________ 23/_____|______________24/     |
+ *              /|      |              /|      |              /|      |
+ *            /  |      |            /  |      |            /  |      |
+ *          /    |      |          /    |      |          /    |      |
+ *       19______|______|_______20______|______|_______21      |      |
+ *        |      |      |        |      |      |        |      |      |
+ *        |      |     16________|______|_____17________|______|_____18
+ *        |      |      /        |      |      /        |      |      /
+ *        |      |    / |        |      |    / |        |      |    / |
+ *        |      |  /   |        |      |  /   |        |      |  /   |
+ *        |     13/_____|________|_____14/_____|________|____15|/     |
+ *        |     /|      |        |     /|      |        |     /|      |
+ *        |   /  |      |        |   /  |      |        |   /  |      |
+ *        | /    |      |        | /    |      |        | /    |      |
+ *       10______|______|_______11______|______|_______12      |      |
+ *        |      |      |        |      |      |        |      |      |
+ *        |      |     7|________|______|____ 8|________|______|____ 9|
+ *        |      |      /        |      |      /        |      |      /
+ *        |      |    /          |      |    /          |      |    /
+ *        |      |  /            |      |  /            |      |  /
+ *        |     4|/______________|____ 5|/______________|____ 6|/
+ *        |     /                |     /                |     /
+ *        |   /                  |   /                  |   /
+ *        | /                    | /                    | /
+ *       1/____________________ 2/____________________ 3/
  * 
  * 
  */
 
 //the time we will wait after lighting one LED (in ms)
-int time = 300;
+int Persistance = 300; // Temps de la persistance retinienne
+int temps = 1000;      // Temps entre 2 changement d'état
 
 void setup(){ //this method will be run once after powering up the arduino
   //put all the pins in output mode so they can light up the LEDs
@@ -75,28 +76,44 @@ void setup(){ //this method will be run once after powering up the arduino
 }
 
 
-void loop(){ //this method will be repeated once the setup is finished
-  splash1(); //we basicly keep calling this method
-}
+void loop(){
 
-void splash1(){
-  all();  //puts on all the LEDs for one second
-  a();  //puts on the first LED
-  b();  //puts on the second LED (turns first one off)
-  c();  //...
-  d();
-  e();
-  f();
-  g();
-  h();
-  i();
-  j();
-  k();
-  l();
-  m();
-  n();
-  o();
-  p();
+  
+  all();  //Allume toutes les led
+  
+  delay (temps); 
+  
+  Led1();  //allume LED N°1
+  delay (temps);
+  Led2();  //allume LED N°2
+  
+  /*Led3();  //...
+  Led4();
+  Led5();
+  Led6();
+  Led7();
+  Led8();
+  Led9();
+  Led10();
+  Led11();
+  Ledl2();
+  Led13();
+  Led14();
+  Led15();
+  Led16();
+  Led17();
+  Led18();
+  Led19();
+  Led20();
+  Led21();
+  Led22();
+  Led23();
+  Led24();
+  Led25();
+  Led26();
+  Led27();
+  */
+  
   off();  //turns all LEDs off
 }
 
@@ -117,226 +134,70 @@ void off(){
   digitalWrite(etage2, LOW);
   digitalWrite(etage3, LOW);
   
-  delay (time);
+  delay (Persistance);
 }
 
-void a(){
-  digitalWrite(colonne1, LOW);
-  digitalWrite(colonne2, HIGH);
-  digitalWrite(colonne3, HIGH);
-  digitalWrite(colonne4, HIGH);
-
-  digitalWrite(colonne5, HIGH);
-  digitalWrite(colonne6, LOW);
-  digitalWrite(colonne7, LOW);
-  digitalWrite(colonne8, LOW);
-  delay (time);
-}
-
-void b(){
+void Led1(){
   digitalWrite(colonne1, HIGH);
   digitalWrite(colonne2, LOW);
-  digitalWrite(colonne3, HIGH);
-  digitalWrite(colonne4, HIGH);
-
-  digitalWrite(colonne5, HIGH);
-  digitalWrite(colonne6, LOW);
-  digitalWrite(colonne7, LOW);
-  digitalWrite(colonne8, LOW);
-  delay (time);
-}
-
-void c(){
-  digitalWrite(colonne1, HIGH);
-  digitalWrite(colonne2, HIGH);
   digitalWrite(colonne3, LOW);
-  digitalWrite(colonne4, HIGH);
-
-  digitalWrite(colonne5, HIGH);
-  digitalWrite(colonne6, LOW);
-  digitalWrite(colonne7, LOW);
-  digitalWrite(colonne8, LOW);
-  delay (time);
-}
-
-void d(){
-  digitalWrite(colonne1, HIGH);
-  digitalWrite(colonne2, HIGH);
-  digitalWrite(colonne3, HIGH);
+  
   digitalWrite(colonne4, LOW);
-
-  digitalWrite(colonne5, HIGH);
+  digitalWrite(colonne5, LOW);
   digitalWrite(colonne6, LOW);
+  
   digitalWrite(colonne7, LOW);
   digitalWrite(colonne8, LOW);
-  delay (time);
+  digitalWrite(colonne9, LOW);
+  
+  digitalWrite(etage1, HIGH);
+  digitalWrite(etage2, LOW);
+  digitalWrite(etage3, LOW);
+  
+  delay (Persistance);
+  off();  //turns all LEDs off
 }
 
-void e(){
+void Led2(){
   digitalWrite(colonne1, LOW);
   digitalWrite(colonne2, HIGH);
-  digitalWrite(colonne3, HIGH);
-  digitalWrite(colonne4, HIGH);
-
-  digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, HIGH);
-  digitalWrite(colonne7, LOW);
-  digitalWrite(colonne8, LOW);
-  delay (time);
-}
-
-void f(){
-  digitalWrite(colonne1, HIGH);
-  digitalWrite(colonne2, LOW);
-  digitalWrite(colonne3, HIGH);
-  digitalWrite(colonne4, HIGH);
-
-  digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, HIGH);
-  digitalWrite(colonne7, LOW);
-  digitalWrite(colonne8, LOW);
-  delay (time);
-}
-
-void g(){
-  digitalWrite(colonne1, HIGH);
-  digitalWrite(colonne2, HIGH);
   digitalWrite(colonne3, LOW);
-  digitalWrite(colonne4, HIGH);
-
-  digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, HIGH);
-  digitalWrite(colonne7, LOW);
-  digitalWrite(colonne8, LOW);
-  delay (time);
-}
-
-void h(){
-  digitalWrite(colonne1, HIGH);
-  digitalWrite(colonne2, HIGH);
-  digitalWrite(colonne3, HIGH);
+  
   digitalWrite(colonne4, LOW);
-
   digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, HIGH);
+  digitalWrite(colonne6, LOW);
+  
   digitalWrite(colonne7, LOW);
   digitalWrite(colonne8, LOW);
-  delay (time);
+  digitalWrite(colonne9, LOW);
+  
+  digitalWrite(etage1, HIGH);
+  digitalWrite(etage2, LOW);
+  digitalWrite(etage3, LOW);
+  
+  delay (Persistance);
+  off();  //turns all LEDs off
 }
 
-void i(){
-  digitalWrite(colonne1, LOW);
-  digitalWrite(colonne2, HIGH);
-  digitalWrite(colonne3, HIGH);
-  digitalWrite(colonne4, HIGH);
 
-  digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, LOW);
-  digitalWrite(colonne7, HIGH);
-  digitalWrite(colonne8, LOW);
-  delay (time);
-}
 
-void j(){
-  digitalWrite(colonne1, HIGH);
-  digitalWrite(colonne2, LOW);
-  digitalWrite(colonne3, HIGH);
-  digitalWrite(colonne4, HIGH);
-
-  digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, LOW);
-  digitalWrite(colonne7, HIGH);
-  digitalWrite(colonne8, LOW);
-  delay (time);
-}
-
-void k(){
-  digitalWrite(colonne1, HIGH);
-  digitalWrite(colonne2, HIGH);
-  digitalWrite(colonne3, LOW);
-  digitalWrite(colonne4, HIGH);
-
-  digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, LOW);
-  digitalWrite(colonne7, HIGH);
-  digitalWrite(colonne8, LOW);
-  delay (time);
-}
-
-void l(){
-  digitalWrite(colonne1, HIGH);
-  digitalWrite(colonne2, HIGH);
-  digitalWrite(colonne3, HIGH);
-  digitalWrite(colonne4, LOW);
-
-  digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, LOW);
-  digitalWrite(colonne7, HIGH);
-  digitalWrite(colonne8, LOW);
-  delay (time);
-}
-
-void m(){
-  digitalWrite(colonne1, LOW);
-  digitalWrite(colonne2, HIGH);
-  digitalWrite(colonne3, HIGH);
-  digitalWrite(colonne4, HIGH);
-
-  digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, LOW);
-  digitalWrite(colonne7, LOW);
-  digitalWrite(colonne8, HIGH);
-  delay (time);
-}
-
-void n(){
-  digitalWrite(colonne1, HIGH);
-  digitalWrite(colonne2, LOW);
-  digitalWrite(colonne3, HIGH);
-  digitalWrite(colonne4, HIGH);
-
-  digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, LOW);
-  digitalWrite(colonne7, LOW);
-  digitalWrite(colonne8, HIGH);
-  delay (time);
-}
-
-void o(){
-  digitalWrite(colonne1, HIGH);
-  digitalWrite(colonne2, HIGH);
-  digitalWrite(colonne3, LOW);
-  digitalWrite(colonne4, HIGH);
-
-  digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, LOW);
-  digitalWrite(colonne7, LOW);
-  digitalWrite(colonne8, HIGH);
-  delay (time);
-}
-
-void p(){
-  digitalWrite(colonne1, HIGH);
-  digitalWrite(colonne2, HIGH);
-  digitalWrite(colonne3, HIGH);
-  digitalWrite(colonne4, LOW);
-
-  digitalWrite(colonne5, LOW);
-  digitalWrite(colonne6, LOW);
-  digitalWrite(colonne7, LOW);
-  digitalWrite(colonne8, HIGH);
-  delay (time);
-}
 
 void all(){
-  digitalWrite(colonne1, LOW);
-  digitalWrite(colonne2, LOW);
-  digitalWrite(colonne3, LOW);
-  digitalWrite(colonne4, LOW);
-
+  digitalWrite(colonne1, HIGH);
+  digitalWrite(colonne2, HIGH);
+  digitalWrite(colonne3, HIGH);
+  
+  digitalWrite(colonne4, HIGH);
   digitalWrite(colonne5, HIGH);
   digitalWrite(colonne6, HIGH);
+
   digitalWrite(colonne7, HIGH);
   digitalWrite(colonne8, HIGH);
-  delay (1000);
+  digitalWrite(colonne9, HIGH);
+
+  digitalWrite(etage1, HIGH);
+  digitalWrite(etage2, HIGH);
+  digitalWrite(etage3, HIGH);
+  
+  delay (temps);
 }      
